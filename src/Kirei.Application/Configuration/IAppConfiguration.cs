f81120aa69ea340bd8a55eee0cc9a4d0cf5b407e
Daniel Belz <daniel_belz@gmx.net>
2019-09-01 22:02:33 +0200

    Adjusted AppConfiguration class and interface.

--- src/Kirei.Application/Configuration/IAppConfiguration.cs
+++ src/Kirei.Application/Configuration/IAppConfiguration.cs
@@ -2,6 +2,7 @@
 {
     public interface IAppConfiguration
     {
+        bool RunOnStartup { get; set; }
         bool HideDesktopIcons { get; set; }
         bool HideTaskbar { get; set; }
         int InactiveStateInMilliseconds { get; set; }
--- src/Kirei.Domain/Models/AppConfiguration.cs
+++ src/Kirei.Domain/Models/AppConfiguration.cs
@@ -4,6 +4,7 @@ namespace Kirei.Configuration
 {
     public class AppConfiguration : IAppConfiguration
     {
+        public bool RunOnStartup { get; set; }
         public bool HideDesktopIcons { get; set; }
         public bool HideTaskbar { get; set; }
         public int InactiveStateInMilliseconds { get; set; }

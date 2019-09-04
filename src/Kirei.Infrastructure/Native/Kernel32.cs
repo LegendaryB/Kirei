2a84a21ebb76a55d8911d6ea476d424e17128c65
Daniel Belz <daniel_belz@gmx.net>
2019-09-04 21:57:59 +0200

    Finished the hibernation service implementation.

---
+++ src/Kirei.Infrastructure/Native/Kernel32.cs
@@ -0,0 +1,12 @@
+﻿using Kirei.Domain.Native.Enums;
+
+using System.Runtime.InteropServices;
+
+namespace Kirei.Infrastructure.Native
+{
+    internal static class Kernel32
+    {
+        [DllImport("kernel32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+        internal static extern ExecutionStates SetThreadExecutionState(ExecutionStates esFlags);
+    }
+}

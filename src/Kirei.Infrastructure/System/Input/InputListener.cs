4bfeb9b3a771c46bd5b4a2453a012356678bd6d3
Daniel Belz <daniel_belz@gmx.net>
2019-09-13 18:48:29 +0200

    Added basic draft of the new implementation

--- src/Kirei.Infrastructure/System/Input/InputListener.cs
+++ src/Kirei.Infrastructure/System/Input/InputListener.cs
@@ -2,6 +2,10 @@
 using Kirei.Infrastructure.Configuration;
 using Kirei.Infrastructure.Native;
 
+using NeatInput;
+
+using System;
+using System.Diagnostics;
 using System.Threading;
 
 namespace Kirei.Infrastructure.System.Input
@@ -9,17 +13,27 @@ namespace Kirei.Infrastructure.System.Input
     public class InputListener :
         IInputListener
     {
+        private InputProvider _inputProvider;
         private IInputActionMapper _inputActionMapper;
-        private bool hasIconsBeenHidden = false;
+
+        private DateTime lastInputReceivedAt;
+        private bool hasIconsBeenHidden = false;        
 
         public void Listen(IInputActionMapper inputActionMapper)
         {
+            _inputProvider = new InputProvider();
             _inputActionMapper = inputActionMapper;
+
+            lastInputReceivedAt = DateTime.Now;
+
+            _inputProvider.InputReceived += OnInputReceived;
 
             while (true)
             {
-                var lastInputInMilliseconds = User32.GetUserIdleTime();
+                var lastInputInMilliseconds = DateTime.Now.Subtract(lastInputReceivedAt).TotalMilliseconds;
                 var inactiveAfterMs = ConfigurationProvider.Configuration.Application.InactiveAfterMs;
+
+                Debug.WriteLine(lastInputInMilliseconds);
 
                 if (lastInputInMilliseconds >= inactiveAfterMs && !hasIconsBeenHidden)
                 {
@@ -34,6 +48,12 @@ namespace Kirei.Infrastructure.System.Input
 
                 Thread.Sleep(ConfigurationProvider.Configuration.Application.InputPollingRate);
             }
+        }
+
+        private void OnInputReceived(NeatInput.Domain.Hooking.Input input)
+        {
+            Debug.WriteLine("INPUT");
+            lastInputReceivedAt = DateTime.Now;
         }
     }
 }

95846d8d369efb0d5bd2f15929cce199eac65527
Daniel Belz <daniel_belz@gmx.net>
2019-09-13 18:49:20 +0200

    Removed GetLastInputInfo

--- src/Kirei.Infrastructure/Native/User32.Wrappers.cs
+++ src/Kirei.Infrastructure/Native/User32.Wrappers.cs
@@ -9,15 +9,6 @@ namespace Kirei.Infrastructure.Native
 {
     internal static partial class User32
     {
-        internal struct LASTINPUTINFO
-        {
-            public uint cbSize;
-            public uint dwTime;
-        }
-
-        [DllImport("user32.dll")]
-        private static extern bool GetLastInputInfo(ref LASTINPUTINFO plii);
-
         [DllImport("user32.dll", CharSet = CharSet.Auto)]
         private static extern int GetClassName(
             IntPtr hWnd,
@@ -39,16 +30,6 @@ namespace Kirei.Infrastructure.Native
         private static extern bool GetWindowRect(
             IntPtr hwnd,
             ref RECT rectangle);
-
-        internal static long GetUserIdleTime()
-        {
-            var lastInputStruct = new LASTINPUTINFO();
-            lastInputStruct.cbSize = (uint)Marshal.SizeOf(lastInputStruct);
-
-            GetLastInputInfo(ref lastInputStruct);
-
-            return ((uint)Environment.TickCount - lastInputStruct.dwTime);
-        }
         
         internal static string GetClassName(IntPtr hWnd)
         {
